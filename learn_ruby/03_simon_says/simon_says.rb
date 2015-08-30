9c339863f853b4d16f70e0fe515e6f5e52717a1a
magicmamba <shivain27@gmail.com>
2015-08-30 00:32:57 -0400

    last three left, done for the night

--- learn_ruby/03_simon_says/simon_says.rb
+++ learn_ruby/03_simon_says/simon_says.rb
@@ -6,4 +6,34 @@ def greet(who)
   "Hello, #{who}!"
 end
 
-TEST
+def echo(text)
+	text
+end 
+
+def shout(text)
+	text.upcase
+end 
+
+def repeat(text,n = 2)
+	([text] * n).join(" ")
+end 
+
+def start_of_word(text,n)
+	text[0..n-1]
+end
+
+def first_word(text)
+	text.split.first
+end
+
+def titleize(text)
+  words = text.split.map do |word|
+    if %w(the and over).include?(word)
+      word
+    else
+      word.capitalize
+    end
+  end
+  words.first.capitalize!
+  words.join(" ")
+end
