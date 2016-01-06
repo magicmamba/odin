b646ca8b144c8586b0539468ed4437b8e918900f
sharpar <Shivain@Macbook-Pro.local>
2016-01-07 00:32:32 +0400

    caesar done

--- rubyprogramming/buidlingblocks/1caesar.rb
+++ rubyprogramming/buidlingblocks/1caesar.rb
@@ -1,4 +1,22 @@
-puts "Enter message to be encrypted"
-text = gets.chomp
 
-puts text 
+def caesar_cipher(text, shift)
+	cipher = ""
+	text.each_char do |i|
+		x = i
+		shift.times do
+			if x == "Z"
+				x = "A"
+				next
+			elsif x == "z"
+				x = "a"
+				next
+			end 
+			x.next!
+			x == "%" ? x = " " : ""
+		end
+	cipher += x
+	end
+	cipher 
+end 
+
+puts caesar_cipher("What a string!", 5)
